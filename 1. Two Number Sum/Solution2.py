ed9118945767606b60090c073514d10bc37b87af
Cheah Eng Teong <ceteongvanness@hotmail.com>
2020-05-15 00:33:26 +0800

    Delete unused file

--- 1. Two Number Sum/Solution2.py
+++
@@ -1,9 +0,0 @@
-def twoNumberSum(array, targetSum):
-    nums = {}
-    for num in array:
-        potentialMatch = targetSum - num
-        if potentialMatch in nums:
-            return [potentialMatch, num]
-        else:
-            nums[num] = True
-    return []
