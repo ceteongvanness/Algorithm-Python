e49a0353a1243b5cc29c7756fb401aa53a32f56b
Cheah Eng Teong <ceteongvanness@hotmail.com>
2020-05-19 17:54:26 +0800

    Binary Search Trees - Right Smaller Than

---
+++ Binary Search Trees/7. Right Smaller Than/Solution.py
@@ -0,0 +1,10 @@
+def rightSmallerThan(array):
+    # Write your code here.
+    rightSmallerCounts = []
+	for i in range(len(array)):
+		rightSmallerCount = 0
+		for j in range(i + 1, len(array)):
+			if array[j] < array[i]:
+				rightSmallerCount += 1
+		rightSmallerCounts.append(rightSmallerCount)
+	return rightSmallerCounts
