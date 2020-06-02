daa050e9d1d3c6e3f653623e5c66760507472b60
Cheah Eng Teong <ceteongvanness@hotmail.com>
2020-06-02 10:26:14 +0800

    Linked Lists - LRU Cache

--- Linked Lists/7. LRU Cache/Solution.py
+++ Linked Lists/7. LRU Cache/Solution.py
@@ -1,33 +1,89 @@
-public class Program {
-	public class LRUCache {
-		public int maxSize;
+class LRUCache:
+    def __init__(self, maxSize):
+		self.cache = {}
+        self.maxSize = maxSize or 1
+		self.currentSize = 0
+		self.listOfMostRecent = DoublyLinkedList()
 
-		public LRUCache(int maxSize) {
-			this.maxSize = maxSize > 1 ? maxSize : 1;
-		}
+	# O(1) time | O(1) space
+    def insertKeyValuePair(self, key, value):
+        if key not in self.cache:
+			if self.currentSize == self.maxSize:
+				self.evictLeastRecent()
+			else:
+				self.currentSize += 1
+			self.cache[key] = DoublyLinkedListNode(key, value)
+		else:
+			self.replaceKey(key, value)
+		self.updateMostRecent(self.cache[key])
 
-		public void InsertKeyValuePair(string key, int value) {
-			// Write your code here.
-		}
+	# O(1) time | O(1) space
+    def getValueFromKey(self, key):
+		if key not in self.cache:
+			return None
+		self.updateMostRecent(self.cache[key])
+		return self.cache[key].value
 
-		public LRUResult GetValueFromKey(string key) {
-			// Write your code here.
-			return null;
-		}
+    def getMostRecentKey(self):
+        return self.listOfMostRecent.head.key
+	
+	def evictLeastRecent(self):
+		keyToRemove = self.listOfMostRecent.tail.key
+		self.listOfMostRecent.removeTail()
+		del self.cache[keyToRemove]
+		
+	def updateMostRecent(self, node):
+		self.listOfMostRecent.setHeadTo(node)
+		
+	def replaceKey(self, key, value):
+		if key not in self.cache:
+			raise Exception("The provided key isn't in the cache!")
+		self.cache[key].value = value
+		
+class DoublyLinkedList:
+	def __init__(self):
+		self.head = None
+		self.tail = None
+		
+	def setHeadTo(self, node):
+		if self.head == node:
+			return
+		elif self.head is None:
+			self.head = node
+			self.tail = node
+		elif self.head == self.tail:
+			self.tail.prev = node
+			self.head = node
+			self.head.next = self.tail
+		else:
+			if self.tail == node:
+				self.removeTail()
+			node.removeBindings()
+			self.head.prev = node
+			node.next = self.head
+			self.head = node
+			
+	def removeTail(self):
+		if self.tail is None:
+			return
+		if self.tail == self.head:
+			self.head = None
+			self.tail = None
+			return
+		self.tail = self.tail.prev
+		self.tail.next = None
 
-		public string GetMostRecentKey() {
-			// Write your code here.
-			return null;
-		}
-	}
-
-	public class LRUResult {
-		public bool found;
-		public int value;
-
-		public LRUResult(bool found, int value) {
-			this.found = found;
-			this.value = value;
-		}
-	}
-}
+class DoublyLinkedListNode:
+	def __init__(self, key, value):
+		self.key = key
+		self.value = value
+		self.prev = None
+		self.next = None
+		
+	def removeBindings(self):
+		if self.prev is not None:
+			self.prev.next = self.next
+		if self.next is not None:
+			self.next.prev = self.prev
+		self.prev = None
+		self.next = None
